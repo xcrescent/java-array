29d32db4112aa87bef1d77c1f63e17bf5b8aa3b8
AKGEC <utkarsh2110024@akgec.ac.in>
2023-05-01 01:13:06 +0530

    Hey

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         int[] intArray = new int[7];
         intArray[0] = 30;
         intArray[1] = 40;
+        intArray[2] = 31;
+        intArray[3] = 41;
+        intArray[4] = 32;
+        intArray[5] = 42;
+        intArray[6] = 33;
         for (int j : intArray) {
             System.out.println(j);
         }
